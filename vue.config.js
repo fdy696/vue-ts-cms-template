0a3dac2de947453b23d9be7cd7c5ce63ffafd214
fdy696 <3456283371@qq.com>
2021-10-09 11:13:50 +0800

    fix: 使用全局引入element-plus临时解决按需引入menu-item style.css失败问题

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,20 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://152.136.185.210:5000',
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  },
   configureWebpack: {
-    plugins: [
-      Components({
-        resolvers: [ElementPlusResolver()]
-      })
-    ],
+    // plugins: [
+    //   Components({
+    //     resolvers: [ElementPlusResolver()]
+    //   })
+    // ],
     resolve: {
       alias: {
         components: '@/components'
